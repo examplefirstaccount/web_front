646534ba8c9eee30e302c7d32e0eeb585aaaaa15
fjfh <anr_prog@mail.ru>
2020-08-02 14:52:15 +0300

    first practical task

--- js/script.js
+++ js/script.js
@@ -12,3 +12,24 @@
 //спец.объекты( массивы[], функции, объекты даты, регулярные выражения, ошибки)
 //обычные объекты
 
+let money = prompt("Ваш бюджет на месяц?", ""),
+        time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+let appData = {
+    budget: money,
+    timeData: time,
+    optionalExpenses: {},
+    expenses: {},
+    income: [],
+    savings: false
+}
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(appData.budget / 30);
